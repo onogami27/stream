ddac0b90c20b32cddaf64ecbbcdc91dd46d09c7f
onogami27 <onogami27@gmail.com>
2021-07-23 06:38:32 +0900

    ファイル追加

---
+++ main.py
@@ -0,0 +1,4 @@
+import sisaku
+
+
+sisaku.rand()
---
+++ sisaku.py
@@ -0,0 +1,13 @@
+import random
+import matplotlib.pyplot as plt
+def rand():
+    list = []
+    for i in range(10):
+        x = random.randint(1,10)
+        list.append(x)
+    
+    y_list = [1,2,3,4,5,6,7,8,9,10]
+
+    plt.plot(list, y_list)
+    plt.show()
+
