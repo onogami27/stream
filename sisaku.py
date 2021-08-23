4b118757f3e9e2070335209a81dd78ac91ad328e
onogami27 <onogami27@gmail.com>
2021-08-24 07:05:49 +0900

    8/24am

--- sisaku.py
+++ sisaku.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import huguai_excel
-
-
+import os
+import datetime
 file = st.file_uploader("ファイルアップロード", type = "xlsx")
 
 if bool(file) == True:
+    time = datetime.datetime.now()
+    file_name = os.path.basename(str(file))
+    print("更新時間:",time,file.name)
+
     x = huguai_excel.excel(file)
 
     st.write(x)
